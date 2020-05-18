d586a02e1009e35a78705c9153f3e1b86b06b5fa
Diechel <Diechel@users.noreply.github.com>
2020-05-18 22:59:37 +0200

    Cleanup

--- esp8266_p1meter.ino
+++ esp8266_p1meter.ino
@@ -31,8 +31,7 @@ Stream* logger;
 // **********************************
 
 // * Gets called when WiFiManager enters configuration mode
-void configModeCallback(WiFiManager *myWiFiManager)
-{
+void configModeCallback(WiFiManager *myWiFiManager){
     logger->println(F("Entered config mode"));
     logger->println(WiFi.softAPIP());
 
@@ -48,8 +47,7 @@ void configModeCallback(WiFiManager *myWiFiManager)
 // **********************************
 
 // * Blink on-board Led
-void tick()
-{
+void tick(){
     // * Toggle state
     int state = digitalRead(LED_BUILTIN);    // * Get the current state of GPIO1 pin
     digitalWrite(LED_BUILTIN, !state);       // * Set pin to the opposite state
@@ -60,33 +58,28 @@ void tick()
 // **********************************
 
 // * Send a message to a broker topic
-void send_mqtt_message(const char *topic, char *payload)
-{
+void send_mqtt_message(const char *topic, char *payload){
     logger->printf("MQTT Outgoing on %s: ", topic);
     logger->println(payload);
 
     bool result = mqtt_client.publish(topic, payload, false);
 
-    if (!result)
-    {
+    if (!result){
         logger->printf("MQTT publish to topic %s failed\n", topic);
     }
 }
 
 // * Reconnect to MQTT server and subscribe to in and out topics
-bool mqtt_reconnect()
-{
+bool mqtt_reconnect(){
     // * Loop until we're reconnected
     int MQTT_RECONNECT_RETRIES = 0;
 
-    while (!mqtt_client.connected() && MQTT_RECONNECT_RETRIES < MQTT_MAX_RECONNECT_TRIES)
-    {
+    while (!mqtt_client.connected() && MQTT_RECONNECT_RETRIES < MQTT_MAX_RECONNECT_TRIES){
         MQTT_RECONNECT_RETRIES++;
         logger->printf("MQTT connection attempt %d / %d ...\n", MQTT_RECONNECT_RETRIES, MQTT_MAX_RECONNECT_TRIES);
 
         // * Attempt to connect
-        if (mqtt_client.connect(HOSTNAME, MQTT_USER, MQTT_PASS))
-        {
+        if (mqtt_client.connect(HOSTNAME, MQTT_USER, MQTT_PASS)){
             logger->println(F("MQTT connected!"));
 
             // * Once connected, publish an announcement...
@@ -96,9 +89,7 @@ bool mqtt_reconnect()
             mqtt_client.publish("hass/status", message);
 
             logger->printf("MQTT root topic: %s\n", MQTT_ROOT_TOPIC);
-        }
-        else
-        {
+        }else{
             logger->print(F("MQTT Connection failed: rc="));
             logger->println(mqtt_client.state());
             logger->println(F(" Retrying in 5 seconds"));
@@ -109,8 +100,7 @@ bool mqtt_reconnect()
         }
     }
 
-    if (MQTT_RECONNECT_RETRIES >= MQTT_MAX_RECONNECT_TRIES)
-    {
+    if (MQTT_RECONNECT_RETRIES >= MQTT_MAX_RECONNECT_TRIES){
         logger->printf("*** MQTT connection failed, giving up after %d tries ...\n", MQTT_RECONNECT_RETRIES);
         return false;
     }
@@ -118,8 +108,7 @@ bool mqtt_reconnect()
     return true;
 }
 
-void send_metric(String name, long metric)
-{
+void send_metric(String name, long metric){
     logger->print(F("Sending metric to broker: "));
     logger->print(name);
     logger->print(F("="));
@@ -132,8 +121,7 @@ void send_metric(String name, long metric)
     send_mqtt_message(topic.c_str(), output);
 }
 
-void send_data_to_broker()
-{
+void send_data_to_broker(){
     send_metric("consumption_low_tarif", CONSUMPTION_LOW_TARIF);
     send_metric("consumption_high_tarif", CONSUMPTION_HIGH_TARIF);
     send_metric("actual_consumption", ACTUAL_CONSUMPTION);
@@ -152,68 +140,60 @@ void send_data_to_broker()
 // * P1                             *
 // **********************************
 
-unsigned int CRC16(unsigned int crc, unsigned char *buf, int len)
-{
-	for (int pos = 0; pos < len; pos++)
-    {
+unsigned int CRC16(unsigned int crc, unsigned char *buf, int len){
+	for (int pos = 0; pos < len; pos++){
 		crc ^= (unsigned int)buf[pos];    // * XOR byte into least sig. byte of crc
                                           // * Loop over each bit
         for (int i = 8; i != 0; i--)
         {
             // * If the LSB is set
-            if ((crc & 0x0001) != 0)
-            {
+            if ((crc & 0x0001) != 0){
                 // * Shift right and XOR 0xA001
                 crc >>= 1;
 				crc ^= 0xA001;
-			}
-            // * Else LSB is not set
-            else
+			}else{
+				// * Else LSB is not set            
                 // * Just shift right
                 crc >>= 1;
+			}
 		}
 	}
 	return crc;
 }
 
-bool isNumber(char *res, int len)
-{
-    for (int i = 0; i < len; i++)
-    {
-        if (((res[i] < '0') || (res[i] > '9')) && (res[i] != '.' && res[i] != 0))
-            return false;
+bool isNumber(char *res, int len){
+    for (int i = 0; i < len; i++){
+        if (((res[i] < '0') || (res[i] > '9')) && (res[i] != '.' && res[i] != 0)){
+			return false;
+		}    
     }
     return true;
 }
 
 int FindCharInArrayRev(char array[], char c, int len)
 {
-    for (int i = len - 1; i >= 0; i--)
-    {
-        if (array[i] == c)
+    for (int i = len - 1; i >= 0; i--){
+        if (array[i] == c){
             return i;
+		}
     }
     return -1;
 }
 
-long getValue(char *buffer, int maxlen, char startchar, char endchar)
-{
+long getValue(char *buffer, int maxlen, char startchar, char endchar){
     int s = FindCharInArrayRev(buffer, startchar, maxlen - 2);
     int l = FindCharInArrayRev(buffer, endchar, maxlen - 2) - s - 1;
 
     char res[16];
     memset(res, 0, sizeof(res));
 
-    if (strncpy(res, buffer + s + 1, l))
-    {
-        if (endchar == '*')
-        {
-            if (isNumber(res, l))
+    if (strncpy(res, buffer + s + 1, l)){
+        if (endchar == '*'){
+            if (isNumber(res, l)){
                 // * Lazy convert float to long
                 return (1000 * atof(res));
-        }
-        else if (endchar == ')')
-        {
+			}
+        }else if (endchar == ')'){
             if (isNumber(res, l))
                 return atof(res);
         }
@@ -221,24 +201,20 @@ long getValue(char *buffer, int maxlen, char startchar, char endchar)
     return 0;
 }
 
-bool decode_telegram(int len)
-{
+bool decode_telegram(int len){
     int startChar = FindCharInArrayRev(telegram, '/', len);
     int endChar = FindCharInArrayRev(telegram, '!', len);
     bool validCRCFound = false;
 
-    for (int cnt = 0; cnt < len; cnt++) {
+    for (int cnt = 0; cnt < len; cnt++){
         logger->print(telegram[cnt]);
     }
     logger->print("\n");
 
-    if (startChar >= 0)
-    {
+    if (startChar >= 0){
         // * Start found. Reset CRC calculation
         currentCRC = CRC16(0x0000,(unsigned char *) telegram+startChar, len-startChar);
-    }
-    else if (endChar >= 0)
-    {
+    }else if (endChar >= 0){
         // * Add to crc calc
         currentCRC = CRC16(currentCRC,(unsigned char*)telegram+endChar, 1);
 
@@ -248,107 +224,92 @@ bool decode_telegram(int len)
         messageCRC[4] = 0;   // * Thanks to HarmOtten (issue 5)
         validCRCFound = (strtol(messageCRC, NULL, 16) == currentCRC);
 
-        if (validCRCFound)
+        if (validCRCFound){
             logger->println(F("CRC Valid!"));
-        else
+        }else{
             logger->println(F("CRC Invalid!"));
+		}
 
         currentCRC = 0;
-    }
-    else
-    {
+    }else{
         currentCRC = CRC16(currentCRC, (unsigned char*) telegram, len);
     }
 
     // 1-0:1.8.1(000992.992*kWh)
     // 1-0:1.8.1 = Elektra verbruik laag tarief (DSMR v4.0)
-    if (strncmp(telegram, "1-0:1.8.1", strlen("1-0:1.8.1")) == 0)
-    {
+    if (strncmp(telegram, "1-0:1.8.1", strlen("1-0:1.8.1")) == 0){
         CONSUMPTION_LOW_TARIF = getValue(telegram, len, '(', '*');
     }
 
     // 1-0:1.8.2(000560.157*kWh)
     // 1-0:1.8.2 = Elektra verbruik hoog tarief (DSMR v4.0)
-    if (strncmp(telegram, "1-0:1.8.2", strlen("1-0:1.8.2")) == 0)
-    {
+    if (strncmp(telegram, "1-0:1.8.2", strlen("1-0:1.8.2")) == 0){
         CONSUMPTION_HIGH_TARIF = getValue(telegram, len, '(', '*');
     }
 
     // 1-0:1.7.0(00.424*kW) Actueel verbruik
     // 1-0:2.7.0(00.000*kW) Actuele teruglevering
     // 1-0:1.7.x = Electricity consumption actual usage (DSMR v4.0)
-    if (strncmp(telegram, "1-0:1.7.0", strlen("1-0:1.7.0")) == 0)
-    {
+    if (strncmp(telegram, "1-0:1.7.0", strlen("1-0:1.7.0")) == 0){
         ACTUAL_CONSUMPTION = getValue(telegram, len, '(', '*');
     }
 
     // 1-0:21.7.0(00.378*kW)
     // 1-0:21.7.0 = Instantaan vermogen Elektriciteit levering
-    if (strncmp(telegram, "1-0:21.7.0", strlen("1-0:21.7.0")) == 0)
-    {
+    if (strncmp(telegram, "1-0:21.7.0", strlen("1-0:21.7.0")) == 0){
         INSTANT_POWER_USAGE = getValue(telegram, len, '(', '*');
     }
 
     // 1-0:31.7.0(002*A)
     // 1-0:31.7.0 = Instantane stroom Elektriciteit
-    if (strncmp(telegram, "1-0:31.7.0", strlen("1-0:31.7.0")) == 0)
-    {
+    if (strncmp(telegram, "1-0:31.7.0", strlen("1-0:31.7.0")) == 0){
         INSTANT_POWER_CURRENT = getValue(telegram, len, '(', '*');
     }
 
     // 0-1:24.2.1(150531200000S)(00811.923*m3)
     // 0-1:24.2.1 = Gas (DSMR v4.0) on Kaifa MA105 meter
-    if (strncmp(telegram, "0-1:24.2.1", strlen("0-1:24.2.1")) == 0)
-    {
+    if (strncmp(telegram, "0-1:24.2.1", strlen("0-1:24.2.1")) == 0){
         GAS_METER_M3 = getValue(telegram, len, '(', '*');
     }
 
     // 0-0:96.14.0(0001)
     // 0-0:96.14.0 = Actual Tarif
-    if (strncmp(telegram, "0-0:96.14.0", strlen("0-0:96.14.0")) == 0)
-    {
+    if (strncmp(telegram, "0-0:96.14.0", strlen("0-0:96.14.0")) == 0){
         ACTUAL_TARIF = getValue(telegram, len, '(', ')');
     }
 
     // 0-0:96.7.21(00003)
     // 0-0:96.7.21 = Aantal onderbrekingen Elektriciteit
-    if (strncmp(telegram, "0-0:96.7.21", strlen("0-0:96.7.21")) == 0)
-    {
+    if (strncmp(telegram, "0-0:96.7.21", strlen("0-0:96.7.21")) == 0){
         SHORT_POWER_OUTAGES = getValue(telegram, len, '(', ')');
     }
 
     // 0-0:96.7.9(00001)
     // 0-0:96.7.9 = Aantal lange onderbrekingen Elektriciteit
-    if (strncmp(telegram, "0-0:96.7.9", strlen("0-0:96.7.9")) == 0)
-    {
+    if (strncmp(telegram, "0-0:96.7.9", strlen("0-0:96.7.9")) == 0){
         LONG_POWER_OUTAGES = getValue(telegram, len, '(', ')');
     }
 
     // 1-0:32.32.0(00000)
     // 1-0:32.32.0 = Aantal korte spanningsdalingen Elektriciteit in fase 1
-    if (strncmp(telegram, "1-0:32.32.0", strlen("1-0:32.32.0")) == 0)
-    {
+    if (strncmp(telegram, "1-0:32.32.0", strlen("1-0:32.32.0")) == 0){
         SHORT_POWER_DROPS = getValue(telegram, len, '(', ')');
     }
 
     // 1-0:32.36.0(00000)
     // 1-0:32.36.0 = Aantal korte spanningsstijgingen Elektriciteit in fase 1
-    if (strncmp(telegram, "1-0:32.36.0", strlen("1-0:32.36.0")) == 0)
-    {
+    if (strncmp(telegram, "1-0:32.36.0", strlen("1-0:32.36.0")) == 0){
         SHORT_POWER_PEAKS = getValue(telegram, len, '(', ')');
     }
 
     return validCRCFound;
 }
 
-void read_p1_serial()
-{
-    if (p1_serial.available())
-    {
+void read_p1_serial(){
+    if (p1_serial.available()){
         memset(telegram, 0, sizeof(telegram));
 
-        while (p1_serial.available())
-        {
+        while (p1_serial.available()){
             ESP.wdtDisable();
             int len = p1_serial.readBytesUntil('\n', telegram, P1_MAXLINELENGTH);
             ESP.wdtEnable(1);
@@ -358,14 +319,11 @@ void read_p1_serial()
     }
 }
 
-void read_p1_hardwareserial()
-{
-    if (Serial.available())
-    {
+void read_p1_hardwareserial(){
+    if (Serial.available()){
         memset(telegram, 0, sizeof(telegram));
 
-        while (Serial.available())
-        {
+        while (Serial.available()){
             ESP.wdtDisable();
             int len = Serial.readBytesUntil('\n', telegram, P1_MAXLINELENGTH);
             ESP.wdtEnable(1);
@@ -375,13 +333,14 @@ void read_p1_hardwareserial()
     }
 }
 
-void processLine(int len) {
+void processLine(int len){
     telegram[len] = '\n';
     telegram[len + 1] = 0;
     yield();
 
     bool result = decode_telegram(len + 1);
-    if (result) {
+	
+    if (result){
         send_data_to_broker();
         LAST_UPDATE_SENT = millis();
     }
@@ -391,29 +350,22 @@ void processLine(int len) {
 // * EEPROM helpers                 *
 // **********************************
 
-String read_eeprom(int offset, int len)
-{
+String read_eeprom(int offset, int len){
     logger->print(F("read_eeprom()"));
 
     String res = "";
-    for (int i = 0; i < len; ++i)
-    {
+    for (int i = 0; i < len; ++i){
         res += char(EEPROM.read(i + offset));
     }
     return res;
 }
 
-void write_eeprom(int offset, int len, String value)
-{
+void write_eeprom(int offset, int len, String value){
     logger->println(F("write_eeprom()"));
-    for (int i = 0; i < len; ++i)
-    {
-        if ((unsigned)i < value.length())
-        {
+    for (int i = 0; i < len; ++i){
+        if ((unsigned)i < value.length()){
             EEPROM.write(i + offset, value[i]);
-        }
-        else
-        {
+        }else{
             EEPROM.write(i + offset, 0);
         }
     }
@@ -426,8 +378,7 @@ void write_eeprom(int offset, int len, String value)
 bool shouldSaveConfig = false;
 
 // * Callback notifying us of the need to save config
-void save_wifi_config_callback ()
-{
+void save_wifi_config_callback(){
     logger->println(F("Should save config"));
     shouldSaveConfig = true;
 }
@@ -436,8 +387,7 @@ void save_wifi_config_callback ()
 // * Setup OTA                      *
 // **********************************
 
-void setup_ota()
-{
+void setup_ota(){
     logger->println(F("Arduino OTA activated."));
 
     // * Port defaults to 8266
@@ -447,23 +397,19 @@ void setup_ota()
     ArduinoOTA.setHostname(HOSTNAME);
     ArduinoOTA.setPassword(OTA_PASSWORD);
 
-    ArduinoOTA.onStart([]()
-    {
+    ArduinoOTA.onStart([](){
         logger->println(F("Arduino OTA: Start"));
     });
 
-    ArduinoOTA.onEnd([]()
-    {
+    ArduinoOTA.onEnd([](){
         logger->println(F("Arduino OTA: End (Running reboot)"));
     });
 
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
-    {
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total){
         logger->printf("Arduino OTA Progress: %u%%\r", (progress / (total / 100)));
     });
 
-    ArduinoOTA.onError([](ota_error_t error)
-    {
+    ArduinoOTA.onError([](ota_error_t error){
         logger->printf("Arduino OTA Error[%u]: ", error);
         if (error == OTA_AUTH_ERROR)
             logger->println(F("Arduino OTA: Auth Failed"));
@@ -485,13 +431,11 @@ void setup_ota()
 // * Setup MDNS discovery service   *
 // **********************************
 
-void setup_mdns()
-{
+void setup_mdns(){
     logger->println(F("Starting MDNS responder service"));
 
     bool mdns_result = MDNS.begin(HOSTNAME);
-    if (mdns_result)
-    {
+    if (mdns_result){
         MDNS.addService("http", "tcp", 80);
     }
 }
@@ -500,13 +444,12 @@ void setup_mdns()
 // * Setup Main                     *
 // **********************************
 
-void setup()
-{
+void setup(){
     // * Configure Serial and EEPROM
     //Serial.begin(BAUD_RATE);
     EEPROM.begin(512);
 
-    if (USE_HARDWARE_SERIAL) {
+    if (USE_HARDWARE_SERIAL){
         // Setup a hw serial connection for communication with the P1 meter
         Serial.begin(BAUD_RATE, SERIAL_8N1, SERIAL_RX_ONLY, 3, true);
 
@@ -516,7 +459,7 @@ void setup()
         ss->enableIntTx(false);
         logger = ss;
         logger->println("Logging using SoftwareSerial has been initialized");
-    } else {
+    }else{
         Serial.println("TODO Software serial not yet implemented!");
         // TODO implement a logger that works for a regular Serial port
         //logger = Serial;
@@ -535,8 +478,7 @@ void setup()
     // * Get MQTT Server settings
     String settings_available = read_eeprom(134, 1);
 
-    if (settings_available == "1")
-    {
+    if (settings_available == "1"){
         read_eeprom(0, 64).toCharArray(MQTT_HOST, 64);   // * 0-63
         read_eeprom(64, 6).toCharArray(MQTT_PORT, 6);    // * 64-69
         read_eeprom(70, 32).toCharArray(MQTT_USER, 32);  // * 70-101
@@ -551,7 +493,7 @@ void setup()
     // * WiFiManager local initialization. Once its business is done, there is no need to keep it around
     WiFiManager wifiManager;
 
-    if (USE_HARDWARE_SERIAL) {
+    if (USE_HARDWARE_SERIAL){
         wifiManager.setDebugOutput(false);
     }
 
@@ -575,8 +517,7 @@ void setup()
 
     // * Fetches SSID and pass and tries to connect
     // * Reset when no connection after 10 seconds
-    if (!wifiManager.autoConnect())
-    {
+    if (!wifiManager.autoConnect()){
         logger->println(F("Failed to connect to WIFI and hit timeout"));
 
         // * Reset and try again, or maybe put it to deep sleep
@@ -591,8 +532,7 @@ void setup()
     strcpy(MQTT_PASS, CUSTOM_MQTT_PASS.getValue());
 
     // * Save the custom parameters to FS
-    if (shouldSaveConfig)
-    {
+    if (shouldSaveConfig){
         logger->println(F("Saving WiFiManager config"));
 
         write_eeprom(0, 64, MQTT_HOST);   // * 0-63
@@ -632,27 +572,22 @@ void loop()
     ArduinoOTA.handle();
     unsigned long now = millis();
 
-    if (!mqtt_client.connected())
-    {
-        if (now - LAST_RECONNECT_ATTEMPT >= 5000)
-        {
+    if (!mqtt_client.connected()){
+        if (now - LAST_RECONNECT_ATTEMPT >= 5000){
             LAST_RECONNECT_ATTEMPT = now;
 
-            if (mqtt_reconnect())
-            {
+            if (mqtt_reconnect()){
                 LAST_RECONNECT_ATTEMPT = 0;
             }
         }
-    }
-    else
-    {
+    }else{
         mqtt_client.loop();
     }
     
-    if (now - LAST_UPDATE_SENT >= UPDATE_INTERVAL) {
-        if (USE_HARDWARE_SERIAL) {
+    if (now - LAST_UPDATE_SENT >= UPDATE_INTERVAL){
+        if (USE_HARDWARE_SERIAL){
             read_p1_hardwareserial();
-        } else {
+        }else{
             read_p1_serial();
         }
     }
